4048ecfa2a362cbec277b1783071978536c29fcb
NatBabi <ntile312@gmail.com>
2025-05-06 01:15:22 +0300

    static HTML for GitHub Pages

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ document
       }
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const formMessage = document.getElementById("form-message");
+  
+    form.addEventListener("submit", function (e) {
+      e.preventDefault(); // stop normal form submit
+  
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
+  
+      fetch("https://formsubmit.co/ajax/ntile312@gmail.com", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          if (!res.ok) throw new Error("Form submit failed");
+          return res.json();
+        })
+        .then(() => {
+          formMessage.textContent = "Message sent successfully!";
+          formMessage.style.color = "#3fa2f6";
+          form.reset();
+        })
+        .catch((error) => {
+          formMessage.textContent = "Failed to send message. Try again.";
+          formMessage.style.color = "red";
+        });
+    });
+  });
+  
